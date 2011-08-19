1c9ed3ebd83b6739c3c4731caee3910bab41a264
Mark Plesko <markplesko@gmail.com>
2011-08-19 16:40:40 -0700

    remove files/directories listed in gitignore sersol

--- target/booklibrary-1.0.0-BUILD-SNAPSHOT/WEB-INF/classes/testdb/schema.sql
+++
@@ -1,5 +0,0 @@
-drop table AUTHOR if exists;
-drop table BOOK if exists;
- 
-CREATE TABLE AUTHOR (ID integer identity primary key, VERSION integer, FIRST_NAME CHAR(80) not null, LAST_NAME CHAR(80) not null, unique(LAST_NAME, FIRST_NAME));
-CREATE TABLE BOOK   (ID integer identity primary key, VERSION integer, AUTHOR_ID integer, TITLE CHAR(80) not null, unique(AUTHOR_ID, TITLE));
